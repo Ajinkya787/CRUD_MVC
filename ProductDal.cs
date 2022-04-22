118b334ce35f0d28c2e2dc4f63825649199d3b3b
ajinkya hake <hakeajinkya2@gmail.com>
2022-04-22 12:53:10 +0530

    Add project files.

---
+++ ProductDal.cs
@@ -0,0 +1,36 @@
+﻿namespace CRUD_MVC
+{
+    public class ProductDal
+    {
+        sqlConnection con;
+        sqlCommon cmd;
+        sqlDataReader dr;
+
+        public ProductDal()
+        {
+            con = new sqlConnection(StartUp.ConnectionString);
+        }
+        public List<Product> GetAllProduct()
+        {
+            string qry = "select * from Product";
+            cmd = new sqlCommand(qry, con);
+            con.Open();
+            dr=cmd.ExecuteReader
+            if (dr.HasRow)
+            {
+                while (dr.Read())
+                {
+                    Product product = new Product();
+                    product.Name = dr["Name"].ToString();
+                    Product.Id = Convert.ToInt32(dr["Id"]);
+                    Product 
+                    int id = (int)dr["id"];
+                    Console.WriteLine(id);
+              }
+                //close the connection 
+                con.close()
+
+            }
+        }
+       }
+}
---
+++ StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD_MVC
+{
+    public class StartUp
+    {
+        public static string ConnectionString { get; set; }
+        IConfiguration Configuration;
+        public StartUp()
+
+        {
+            ConnectionString = Configuration.GetValue<string>("Server=DESKTOP-3B7G3J0\SQLEXPRESS01");
+        }
+    }
+}
